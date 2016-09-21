650f4533d95f11eb2c2f7de75df236def722999b
Mettjus <karel291180@gmail.com>
2016-09-21 22:04:08 +0200

    better example

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 const _ = require('lodash')
 
 console.log( _.camelCase('hello world') )
+
+let div = document.createElement('div')
+div.innerHTML = _.camelCase('hello world')
+document.body.appendChild(div)
